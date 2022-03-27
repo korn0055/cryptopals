897ee50c5686fd019b6e456267713faf4324737d
Ryan Kornetzke <korn0055@gmail.com>
2022-03-27 15:18:47 -0500

    load and encrypt random plaintext for challenge 17

---
+++ cbc_padding_oracle.py
@@ -0,0 +1,22 @@
+import aes_custom as aes
+import base64
+import os
+import random
+
+def get_random_encrypted_line():
+    iv = os.urandom(16)
+    # key is created once
+    fixed_key = aes.RANDOM_AES_KEY
+
+    with open("set3challenge17.txt", 'rb') as f:
+        lines = f.readlines()
+        random_line = random.choice(lines)
+        plaintext = base64.decodebytes(random_line)
+        padded_plaintext = aes.pad_pkcs7(plaintext)
+        encrypted = aes.encrypt_cbc(padded_plaintext, fixed_key, iv)
+        return encrypted, iv
+
+if __name__ == "__main__":
+    print("set3, challenge 17 - CBC Padding Oracle")
+    ciphertext, iv = get_random_encrypted_line()
+    print(f"ciphertext={ciphertext.hex()}, iv={iv.hex()}")
