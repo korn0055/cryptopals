613f104b660684e76d7ba1e80b6ce3d1bd125cc0
Ryan Kornetzke <korn0055@gmail.com>
2022-03-27 20:46:03 -0500

    Update cbc_padding_oracle.py
    Add padding oracle function

--- cbc_padding_oracle.py
+++ cbc_padding_oracle.py
@@ -1,3 +1,4 @@
+from sympy import false, true
 import aes_custom as aes
 import base64
 import os
@@ -16,7 +17,18 @@ def get_random_encrypted_line():
         encrypted = aes.encrypt_cbc(padded_plaintext, fixed_key, iv)
         return encrypted, iv
 
+def padding_oracle(ciphertext, iv):
+    decrypted = aes.decrypt_cbc(ciphertext, aes.RANDOM_AES_KEY, iv)
+    try:
+        aes.unpad_pkcs7(decrypted)
+        return true
+    except:
+        return false
+
+
 if __name__ == "__main__":
     print("set3, challenge 17 - CBC Padding Oracle")
     ciphertext, iv = get_random_encrypted_line()
     print(f"ciphertext={ciphertext.hex()}, iv={iv.hex()}")
+    is_padding_valid = padding_oracle(ciphertext, iv)
+    print(f"is_padding_valid={is_padding_valid}")
