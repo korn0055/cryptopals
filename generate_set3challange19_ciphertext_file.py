3e5b17359453e4f81d4bdf16c702d0b84ee05160
Ryan Kornetzke <korn0055@gmail.com>
2022-04-17 10:52:54 -0500

    attack mostly working

--- generate_set3challange19_ciphertext_file.py
+++ generate_set3challange19_ciphertext_file.py
@@ -2,10 +2,11 @@ import aes_custom
 import base64
 
 if __name__ == "__main__":
+    # take the plaintext base64 encoded file, encrypt it line-by-line, and save the output to a new base64 
     with open('set3challenge19_pt.txt', 'rb') as f_in, open('set3challenge19_ct.txt', 'wb') as f_out:
         for line in f_in:
             f_out.write(base64.encodebytes(aes_custom.encrypt_ctr(base64.decodebytes(line), key=aes_custom.RANDOM_AES_KEY, nonce=0)))
-        
+    # decrypted the new file and make sure it matches the original
     with open('set3challenge19_pt.txt', 'rb') as f_pt, open('set3challenge19_ct.txt', 'rb') as f_ct:
         for pt, ct in zip(f_pt, f_ct):
             ciphertext = base64.decodebytes(ct)
--- set3challenge19.py
+++ set3challenge19.py
@@ -1,39 +1,50 @@
 from pydoc import plain
-import aes_custom
 import base64
 from break_repeating_key_xor import score_over_keyspace
-import numpy as np
 import random
 import bit_utils
+import itertools
 
-def vertical_slices(x):
-    test_vals = [bytes(list(range(random.randint(6,20)))) for i in range(10)]
+# just some sanity checking to make sure things are being sliced correctly
+def test_vertical_slices():
+    test_vals = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"[:random.randint(6,27)] for i in range(10)]
     print(test_vals)
 
     max_len = max(map(len, test_vals))
     print(f"max_len={max_len}")
-    return list(zip(*test_vals))
+    # the output of standard zip is only as long as the shortest input, zip_longest pads with None
+    slices = list(itertools.zip_longest(*test_vals))
+    for s in slices:
+        print(f"slice={s}")
 
-    
-
-
+# we know that each line of the ciphertext is is encrypted with CTR mode and uses the same nonce
+# this means the keystream byte that is xor-ed with the plaintext is the same for each "column"
+# plan is to slice the ciphertext into columns and do letter frequency analysis along each column
+# the rows not being the same lenght makes it a little trickier - some may not have enough ciphertext
+# for frequency analysis to be useful
 if __name__ == "__main__":
-    ct = open('set3challenge19_ct.txt', 'rb').readlines()
-    print('\n'.join([x.hex() for x in ct]))
+    test_vertical_slices()
+    ct = []
+    with open('set3challenge19_ct.txt', 'rb') as f:
+        for line in f:
+            ct_line = base64.decodebytes(line)
+            print(f"ct_line_len={len(ct_line)}")
+            ct += [ct_line]
     
     # slice the ciphertexts by keystream position/counter value
-    # try all keysteam values and calculate a score
     keys_by_slice = []
-    # slices = vertical_slices(ct)
     slices = list(zip(*ct))
     # print(slices)
     for slice in slices:
+        # try all key values and calculate a score
         slice_bytes = bytes(slice)
-        print(f"slice={slice_bytes.hex()}")
-        scores = score_over_keyspace(bytes(slice_bytes), return_dict=True)
-        # print(f"scores={scores}")
-        keys_by_slice.append(sorted(scores, key=scores.get, reverse=True)[:1])
+        print(f"len_slice_bytes={len(slice_bytes)}")
+        # print(f"slice={slice_bytes.hex()}")
+        scores = score_over_keyspace(slice_bytes, return_dict=True)
+        # print(f"scores={sorted(scores, key=scores.get, reverse=True)}")
+        keys_by_slice.append(sorted(scores, key=scores.get, reverse=True))
 
+    # print(f"keys_by_slice={keys_by_slice}")
     keystream = bytes(list(zip(*keys_by_slice))[0])
     print(f"keystream={keystream}")
     for line in ct:
